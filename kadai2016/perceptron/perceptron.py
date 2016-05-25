aaed916d21f7b103b0c1f145f27b22306bde9f81
suzuki-13050 <sd13050@toyota-ti.ac.jp>
2016-05-25 19:56:23 +0900

    高速化

--- kadai2016/perceptron/perceptron.py
+++ kadai2016/perceptron/perceptron.py
@@ -1,17 +1,17 @@
-import sys,math
+import sys
 
 def read_instance(sentences):#1レビュー分のインスタンス作成
     bow_list=[]  #素性ベクトル格納用リスト
     flg = False
     
-    for sentence in sentences.split(" "):
+    for sentence in sentences.split():
         if flg == False:#1ループ目=ラベル
             flg = True
-            rabel=sentence
+            rabel=int(sentence)
         else:
             if ":" in sentence :#素性ベクトルがあるときのみ
                 key_value=sentence.split(":")#「：」の左側=インデックス、右側=頻度
-                bow_list.append((key_value[0],key_value[1]))
+                bow_list.append((int(key_value[0]),int(key_value[1])))
                 
     return (rabel,bow_list)#1レビュー分のインスタンス
 
@@ -25,59 +25,54 @@ def read_data(file_name):#レビューデータ全体のインスタンス作成
 
     for j in range(len(all_data)):
         #各レビューにおける最大インデックスと現在の最大インデックスを比較
-        max_index=max(int(max(all_data[j][1],key=lambda x: int(x[0]))[0]),max_index)
-            
+        max_index=max(max(all_data[j][1],key=lambda x: x[0])[0],max_index)
+        
     return (all_data,max_index)#(入出力データ,インデックスの最大値)のタプル
 
 def add_fv(one_review):#重みベクトルに素性ベクトルを足す
-    for i in range(len(one_review)):
-        weight[int(one_review[i][0])]+=int(one_review[i][1])#インデックスと同じところに頻度を足す 
+    for instance in one_review:
+        weight[instance[0]]+=instance[1]
 
 def sub_fv(one_review):#重みベクトルから素性ベクトルを引く
-    for i in range(len(one_review)):
-        weight[int(one_review[i][0])]-=int(one_review[i][1])#インデックスと同じところに頻度を足す
+    for instance in one_review:
+        weight[instance[0]]-=instance[1]#インデックスと同じところから頻度を引く
     
-def mult_fv(one_review):#重みベクトルと素性ベクトルの内積計算
+def mult_fv(one_review,length):#重みベクトルと素性ベクトルの内積計算
     answer=0
-    for i in range(len(one_review)):#レビュー内の全インデックス分計算
-        if len(weight) > int(one_review[i][0]):#重みベクトルの長さ以内のインデックスである場合
-            answer+=weight[int(one_review[i][0])]*int(one_review[i][1])
 
+    for index,fv in one_review:#レビュー内の全インデックス分計算
+        if length > index:#重みベクトルの長さ以内のインデックスである場合
+            answer+=weight[index]*fv
+            
     return answer
 
-def update_weight(review_data):#重みベクトルの更新
-    for one_review in review_data:
-        naiseki=mult_fv(one_review[1])#素性ベクトルを渡して内積計算
-        if (naiseki*int(one_review[0])) <= 0:#内積値とラベルが異符号の時のみ重みベクトルの更新を行う
-            #print("ラベル：",one_review[0],"内積値：",naiseki)
-            if int(one_review[0]) > 0:#ラベルが正
-                add_fv(one_review[1])
-            else:
-                sub_fv(one_review[1])
-            
-def evaluate(review_data):#正解率の算出
+def update_weight(label,one_review,length):#重みベクトルの更新
+    #素性ベクトルを渡して内積計算
+    if (mult_fv(one_review,length)*label) <= 0:#内積値とラベルが異符号の時のみ重みベクトルの更新を行う
+        if label > 0:#ラベルが正
+            add_fv(one_review)
+        else:
+            sub_fv(one_review)
+    
+def evaluate(review_data,length):#正解率の算出
     correct=0
     for one_review in review_data:
-        naiseki=mult_fv(one_review[1])#素性ベクトルを渡して内積計算
-        if (naiseki*int(one_review[0])) > 0:#内積値とラベルが同符号=正解
+        naiseki=mult_fv(one_review[1],length)#素性ベクトルを渡して内積計算
+        if (naiseki*one_review[0]) > 0:#内積値とラベルが同符号=正解
             correct+=1
-    rate=correct/len(review_data)
-            
-    return rate #Accuracy rate=正解率
-                
+    return correct,len(review_data),(correct/len(review_data)) #Accuracy rate=正解率=正解数÷インスタンス数
+
 if __name__ == "__main__":
     train_data,max_index=read_data(sys.argv[1])
-    test_data,max_index_test=read_data(sys.argv[2])
+    
     #print(max_index,max_index_test)#教師データとテストデータの最大インデックス
     weight=[0]*(max_index+1)#重みベクトルの宣言(長さ=レビュー数+1(バイアス項))
-    #sub_fv(train_data[1][1])
     
     for learned_count in range(int(sys.argv[3])):
-        #print("学習回数",learned_count+1)
-        update_weight(train_data)
-        
-    #print(weight[0:2000])
-    #print(train_data)
-        
-    print("正解率:"+str(evaluate(test_data)*100)+"%")
+        for instances in train_data:
+            update_weight(instances[0],instances[1],max_index)
 
+    test_data,max_index_test=read_data(sys.argv[2])
+    correct_n,instance_n,accuracy_rate=evaluate(test_data,max_index)
+    #print(weight[0:2000])
+    print("正解数:"+str(correct_n),"インスタンス数:"+str(instance_n),"正解率:"+str(accuracy_rate*100)+"%")
